1405087cb098f48e6ddc827988c38f1a9a375980
Kugel-qt <noeljonathan94@gmail.com>
2022-09-26 05:33:42 +0200

    add an isHeld property to dice

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,13 +5,16 @@ export default function Main() {
     function getNewDice(numberOfDice) {
         const newDiceArr = []
         for (let i = 0; i < numberOfDice; i++) {
-            newDiceArr.push(Math.ceil(Math.random() * 6))
+            newDiceArr.push({
+                value: (Math.ceil(Math.random() * 6)),
+                isHeld: false
+            })
         }
         return newDiceArr
     }
 
     const [dice, setDice] = React.useState(getNewDice(10))
-    const diceElements = dice.map( die => <Die value={die} />)
+    const diceElements = dice.map( die => <Die value={die.value} />)
     
     function handleClick() {
         setDice(getNewDice(10))
